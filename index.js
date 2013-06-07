5bb34a562bf22899974d468720da1399674336d8
Adam Daniel <adam@acdaniel.com>
2013-06-07 01:49:47 -0400

    removes potential issue with older IEs 31 style tag limit

--- index.js
+++ index.js
@@ -5,26 +5,35 @@ var less = require('less'),
 exports.extension = ['less'];
 exports.noDependencies = true;
 
-function buildCode(css) {
-  var formattedCss = css.replace(/\"/g, '\\"').trim().replace(/\n/g, '\\n" + \n"');
-  var code =
-    'var src = "' + formattedCss + '";\n' +
-    'var style = document.getElementsByTagName("style")[0] || document.createElement("style"); style.type="text/css"; style.appendChild(document.createTextNode(src)); document.getElementsByTagName("head")[0].appendChild(style);\n' +
-    'module.exports = {src: src, element: style};';
+function buildCode(css, info) {
+  var formattedCss = css.replace(/\"/g, '\\"').trim().replace(/\n/g, '\\n" + \n"'),
+      file = info.localFilename,
+      code =
+        'var element, src = "/* begin: ' + file + ' */\\n" +\n"' + formattedCss + '\\n" + \n"/* end: ' + file + ' */";\n' +
+        'var style, styles = document.getElementsByTagName("style");\n' +
+        'if (styles.length > 0) {\n' +
+        '  style = styles[styles.length-1];\n' +
+        '} else {\n' +
+        '  style = document.createElement("style");\n' +
+        '  style.type="text/css";\n' +
+        '  document.getElementsByTagName("head")[0].appendChild(style);\n' +
+        '}\n' +
+        'style.appendChild(document.createTextNode(src));\n' +
+        'module.exports = {src: src, element: element, file: "' + file + '"};';
   return code;
 }
 
-function compileLess(filename, src) {
+function compileLess(src, info) {
   var def = deferred();
-  less.render(src, {compress: true, paths: [path.dirname(filename)]}, function (err, css) {
+  less.render(src, {compress: true, paths: [path.dirname(info.filename)], dumpLineNumbers: 'mediaquery'}, function (err, css) {
     if (err) {
       return def.reject(new Error(err));
     }
-    return def.resolve(buildCode(css));
+    return def.resolve(buildCode(css, info));
   });
   return def.promise;
 }
 
 exports.compile = function (src, info) {
-  return { code: compileLess(info.filename, src) };
+  return { code: compileLess(src, info) };
 };
