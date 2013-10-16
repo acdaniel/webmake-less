68f5726f8c543804f27132815932bd2a139a719a
Adam Daniel <adam@acdaniel.com>
2013-10-16 09:03:06 -0400

    updates to use the new css type for webmake

--- index.js
+++ index.js
@@ -3,25 +3,8 @@ var less = require('less'),
     deferred = require('deferred');
 
 exports.extension = ['less'];
+exports.type = 'css';
 exports.noDependencies = true;
-
-function buildCode(css, info) {
-  var formattedCss = css.replace(/\"/g, '\\"').trim().replace(/\n/g, '\\n" + \n"'),
-      file = info.localFilename,
-      code =
-        'var element, src = "/* begin: ' + file + ' */\\n" +\n"' + formattedCss + '\\n" + \n"/* end: ' + file + ' */";\n' +
-        'var style, styles = document.getElementsByTagName("style");\n' +
-        'if (styles.length > 0) {\n' +
-        '  style = styles[styles.length-1];\n' +
-        '} else {\n' +
-        '  style = document.createElement("style");\n' +
-        '  style.type="text/css";\n' +
-        '  document.getElementsByTagName("head")[0].appendChild(style);\n' +
-        '}\n' +
-        'style.appendChild(document.createTextNode(src));\n' +
-        'module.exports = {src: src, element: element, file: "' + file + '"};';
-  return code;
-}
 
 function compileLess(src, info) {
   var def = deferred();
@@ -29,7 +12,7 @@ function compileLess(src, info) {
     if (err) {
       return def.reject(new Error(err));
     }
-    return def.resolve(buildCode(css, info));
+    return def.resolve(css);
   });
   return def.promise;
 }
